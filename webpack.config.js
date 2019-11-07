a95a67182390b6f35be292e40cea838db607a277
Faruq <lekside1@gmail.com>
2019-11-07 14:08:18 -0500

    Added webpack config

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const webpack = require('webpack');
+
+module.exports = {
+    mode: 'development',
+    entry: './index.js',
+    output: {
+        path: __dirname,
+        filename: './bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            loader: 'babel-loader'
+        }]
+    }
+}; 
