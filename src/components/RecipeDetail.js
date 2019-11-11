486a17bc893e07faa66f625f38c24978a169175b
Faruq <lekside1@gmail.com>
2019-11-11 13:35:37 -0500

    Setting up ESlint

--- src/components/RecipeDetail.js
+++ src/components/RecipeDetail.js
@@ -1,47 +1,65 @@
 import React from 'react';
 import classNames from 'classnames';
+import PropTypes from 'prop-types';
 
 const RecipeDetail = (props) => {
-    if(!props.recipe) {
-        return (
-            <p 
-                style={props.style}
-                className={classNames('h3 p2 bg-white italic center', props.className)}
-            >
-                Please select a recipe to see the detail.
-            </p>
-        );
-    }
-
+  if (!props.recipe) {
     return (
-        <div 
-            style={props.style}
-            className={classNames('p2 bg-white', props.className)}
-        >
-            <h2 className="h2">{props.recipe.name}</h2>
-            <img className="fit" src={props.recipe.image}/>
-            <div>
-                <span>{props.recipe.category} </span>
-                <span>{props.recipe.calories} cal</span>
-            </div>
-            <h3>Ingredients</h3>
-            <ul>
-                {props.recipe.ingredients.map(ingredient => (
-                    <li key={ingredient}>
-                        {ingredient}
-                    </li>
-                ))}
-            </ul>
-            <h3>Steps</h3>
-            <ol>
-                {props.recipe.steps.map(step => (
-                    <li key={step}>
-                        {step}
-                    </li>
-                ))}
-            </ol>
-        </div>
+      <p
+        style={props.style}
+        className={classNames('h3 p2 bg-white italic center', props.className)}
+      >
+                Please select a recipe to see the detail.
+      </p>
     );
+  }
+
+  return (
+    <div
+      style={props.style}
+      className={classNames('p2 bg-white', props.className)}
+    >
+      <h2 className="h2">{props.recipe.name}</h2>
+      <img
+        className="fit"
+        src={props.recipe.image}
+        alt={props.recipe.name}
+      />
+      <div>
+        <span>
+          {props.recipe.category}
+          {' '}
+        </span>
+        <span>
+          {props.recipe.calories}
+          {' '}
+          cal
+        </span>
+      </div>
+      <h3>Ingredients</h3>
+      <ul>
+        {props.recipe.ingredients.map((ingredient) => (
+          <li key={ingredient}>
+            {ingredient}
+          </li>
+        ))}
+      </ul>
+      <h3>Steps</h3>
+      <ol>
+        {props.recipe.steps.map((step) => (
+          <li key={step}>
+            {step}
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+};
+
+RecipeDetail.propTypes = {
+  recipe: PropTypes.object,
+  style: PropTypes.object,
+  className: PropTypes.string,
 };
 
 export default RecipeDetail;
--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,21 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const RecipeList = (props) => (
-    <div style={props.style}>
-       <h2>Recipes</h2>
-       <ul className="list-reset">
-            {props.recipes.map(recipe => (
-                <li 
-                    key={recipe.id} 
-                    className="py2 border-bottom border-bottom-dashed pointer"
-                    onClick={() => props.onClick(recipe.id)}
-                >
-                   <span>{recipe.name} </span> 
-                   <span>{recipe.category}</span>
-               </li>
-            ))}
-       </ul>
-    </div>
+  <div style={props.style}>
+    <h2>Recipes</h2>
+    <ul className="list-reset">
+      {props.recipes.map((recipe) => (
+        <li
+          key={recipe.id}
+          className="py2 border-bottom border-bottom-dashed pointer"
+          onClick={() => props.onClick(recipe.id)}
+        >
+          <span>
+            {recipe.name}
+            {' '}
+          </span>
+          <span>{recipe.category}</span>
+        </li>
+      ))}
+    </ul>
+  </div>
 );
+
+RecipeList.propTypes = {
+  style: PropTypes.object,
+  recipes: PropTypes.object,
+  onClick: PropTypes.object,
+};
 
 export default RecipeList;
