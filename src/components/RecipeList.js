32c43e1935f1c1ee94a1369ddaf4d17d3f441d7f
Faruq <lekside1@gmail.com>
2019-11-11 12:45:43 -0500

    Styling with basscss

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -3,10 +3,11 @@ import React from 'react';
 const RecipeList = (props) => (
     <div style={props.style}>
        <h2>Recipes</h2>
-       <ul>
+       <ul className="list-reset">
             {props.recipes.map(recipe => (
                 <li 
                     key={recipe.id} 
+                    className="py2 border-bottom border-bottom-dashed pointer"
                     onClick={() => props.onClick(recipe.id)}
                 >
                    <span>{recipe.name} </span> 
