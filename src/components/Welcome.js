155ba659e5aa0a65f2458532d607989e4fec35b4
Faruq <lekside1@gmail.com>
2019-12-16 10:48:01 -0500

    Added template for redux (reducer, action, store | use Hooks in Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,23 +1,17 @@
-import React from 'react';
+// import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Spring } from 'react-spring/renderprops';
 import { Switch, Redirect } from 'react-router-dom';
 
+const Welcome = () => {
+  const [redirect, setRedirect] = useState(false);
 
-class Welcome extends React.Component {
-  constructor(props) {
-    super(props);
+  useEffect(() => {
+    setTimeout(() => setRedirect(true), 3000);
+  });
 
-    this.state = {
-      redirect: false,
-    };
-  }
-
-  componentDidMount() {
-    this.id = setTimeout(() => this.setState({ redirect: true }), 3000);
-  }
-
-  render() {
-    return this.state.redirect
+  return (
+    redirect
       ? <Switch><Redirect to="/home" /></Switch>
       : (
         <Spring
@@ -31,8 +25,42 @@ class Welcome extends React.Component {
             </div>
           )}
         </Spring>
-      );
-  }
-}
+      )
+  );
+};
 
 export default Welcome;
+
+// class Welcome extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       redirect: false,
+//     };
+//   }
+
+//   componentDidMount() {
+//     setTimeout(() => this.setState({ redirect: true }), 3000);
+//   }
+
+//   render() {
+//     return this.state.redirect
+//       ? <Switch><Redirect to="/home" /></Switch>
+//       : (
+//         <Spring
+//           from={{ opacity: 0, marginLeft: -500 }}
+//           to={{ opacity: 1, marginLeft: 0 }}
+//           config={{ delay: 500, duration: 1000 }}
+//         >
+//           {props => (
+//             <div>
+//               <h1 style={props} className="h1 center bg-gray">Welcome to MyRecipes App</h1>
+//             </div>
+//           )}
+//         </Spring>
+//       );
+//   }
+// }
+
+// export default Welcome;
