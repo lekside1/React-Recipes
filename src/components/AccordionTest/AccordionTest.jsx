9930d8720fea74c0abec04b1fbbb470c43878bb7
Faruq <lekside1@gmail.com>
2020-09-11 20:12:32 -0400

    update Readme and AccordionTest.jsx files

--- src/components/AccordionTest/AccordionTest.jsx
+++ src/components/AccordionTest/AccordionTest.jsx
@@ -5,13 +5,13 @@ const AccordionTest = () => (
   <div className="ml4 mr4">
     <h1 className="h2">Scenario Selector - Click to apply</h1>
     <Accordion>
-      <div label="BBAD Training Scenarios">
-        <span>Day 1 Training</span>
+      <div label="Option 1">
+        <span>Sub option 1</span>
         {/* <span>Day 2 Training</span>
         <span>version 1.0</span> */}
       </div>
-      <div label="My Scenarios">
-        <span>FGC Lesson</span>
+      <div label="Option 2">
+      <span>Sub option 1</span>
         {/* <span>MDC-Lesson</span> */}
       </div>
     </Accordion>
